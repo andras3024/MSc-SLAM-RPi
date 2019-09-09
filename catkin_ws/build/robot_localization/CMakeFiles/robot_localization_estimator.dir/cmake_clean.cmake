be2bf5a34c900fef6326a6f618cf10412dd04681
Ludvig Andras Raspi <andras.ludvig@yahoo.de>
2019-09-09 17:19:40 +0200

    Autostart

---
+++ catkin_ws/build/robot_localization/CMakeFiles/robot_localization_estimator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_localization_estimator.dir/src/robot_localization_estimator.cpp.o"
+  "/home/ubuntu/Documents/GitHub/MSc-SLAM-RPi/catkin_ws/devel/.private/robot_localization/lib/librobot_localization_estimator.pdb"
+  "/home/ubuntu/Documents/GitHub/MSc-SLAM-RPi/catkin_ws/devel/.private/robot_localization/lib/librobot_localization_estimator.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_localization_estimator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/robot_localization/catkin_generated/robot_localization-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(robot_localization_MESSAGE_FILES "")
+set(robot_localization_SERVICE_FILES "/home/ubuntu/Documents/GitHub/MSc-SLAM-RPi/catkin_ws/src/robot_localization/srv/GetState.srv;/home/ubuntu/Documents/GitHub/MSc-SLAM-RPi/catkin_ws/src/robot_localization/srv/SetDatum.srv;/home/ubuntu/Documents/GitHub/MSc-SLAM-RPi/catkin_ws/src/robot_localization/srv/SetPose.srv;/home/ubuntu/Documents/GitHub/MSc-SLAM-RPi/catkin_ws/src/robot_localization/srv/ToggleFilterProcessing.srv")
